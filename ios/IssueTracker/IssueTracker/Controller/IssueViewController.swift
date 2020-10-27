3a3e1f9b0f5801e51c282733954d6ca453ac4440
NamKiBeom <nkb5813@naver.com>
2020-10-27 16:20:39 +0900

    [feat] IssueViewController UICollectionView layout setting

---
+++ ios/IssueTracker/IssueTracker/Controller/IssueViewController.swift
@@ -0,0 +1,27 @@
+//
+//  IssueViewController.swift
+//  IssueTracker
+//
+//  Created by 남기범 on 2020/10/27.
+//  Copyright © 2020 남기범. All rights reserved.
+//
+
+import UIKit
+
+class IssueViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension IssueViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
