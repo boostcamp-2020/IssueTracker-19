785e79be997b9f088e7ab52ea314cf0e8a75f79f
rnjshippo <tkstka0023@naver.com>
2020-10-27 20:04:36 +0900

    [feat] userModel - addUser 작성
    1. id, nickname, auth 값으로 유저 추가 구현
    2. AUTH 관련 constants 생성

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,8 +1,13 @@
 import pool from '@lib/db';
+import { AUTH } from '@lib/constants';
 
 export const userModel = {
   getUserById({ id }) {
     const sql = 'SELECT * FROM user WHERE id=?;';
     return pool.execute(sql, [id]);
+  },
+  addUser({ id, nickname, auth }) {
+    const sql = 'INSERT INTO user (id, nickname, auth) VALUES (?, ?, ?);';
+    return pool.execute(sql, [id, nickname, auth ? auth : AUTH.DEFAULT]);
   },
 };
