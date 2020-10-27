dfbf05886b6b819fc1181cedee0fcf8f52195cc0
NamKiBeom <nkb5813@naver.com>
2020-10-27 17:20:33 +0900

    [feat] milestone, label UI change

--- ios/IssueTracker/IssueTracker/Controller/IssueViewController.swift
+++ ios/IssueTracker/IssueTracker/Controller/IssueViewController.swift
@@ -11,17 +11,37 @@ import UIKit
 class IssueViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
+    private enum Section: CaseIterable {
+        case issue
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.register(IssueCollectionViewCell.self, forCellWithReuseIdentifier: "IssueCell")
+        
+        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flowLayout.estimatedItemSize = CGSize(width: collectionView.frame.width, height: 100)
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        
+        collectionView.collectionViewLayout = flowLayout
+        collectionView.backgroundColor = UIColor.systemGray5
+        
     }
 }
 
 extension IssueViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "IssueCell",
+                                                            for: indexPath) as? IssueCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
     }
 }
