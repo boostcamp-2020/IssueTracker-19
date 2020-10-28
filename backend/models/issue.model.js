41e00dbcdfe99f80c4dffafb5987783e79f872bb
rnjshippo <tkstka0023@naver.com>
2020-10-28 23:09:36 +0900

    [feat] issue list 조회 model/controller 구현
    관련 model 작성완료
    
    issues에 필요한 정보를 join해서 가져온 배열에 해당 이슈에 맞는 labels와 assignees를 추가하여 반환하는 코드 구현

--- backend/models/issue.model.js
+++ backend/models/issue.model.js
@@ -4,18 +4,37 @@ import pool from '@lib/db';
 
 export const issueModel = {
   /* prettier-ignore */
-  getIssueList({ isOpened, author, assignee, milestone, comment, label, keyword }) {
-    const sql = 'SELECT * FROM issue WHERE open=?;';
-    return pool.execute(sql, [open]);
+  getIssueList() {
+    const sql = `SELECT i.no, u.nickname as author, i.title, i.is_opened as isOpened, i.created_at as createdAt, 
+    i.closed_at as closedAt, m.title as milestone, (SELECT COUNT(comment.no) FROM comment WHERE issue_no=i.no) as commentCount 
+    FROM issue i 
+    LEFT JOIN milestone m ON m.no = i.milestone_no
+    LEFT JOIN user u ON u.no = i.author_no
+    ORDER BY i.no;`;
+    return pool.query(sql);
   },
   getIssueByNo({ no }) {
-    const sql = 'SELECT * FROM issue WHERE no=';
-    return pool.execute(sql);
+    const sql = 'SELECT * FROM issue WHERE no=?;';
+    return pool.execute(sql, [no]);
   },
-  addIssue({ authorNo, title, milestoneNo }) {
-    const issueSql =
+  addIssue({ title, authorNo, milestoneNo }) {
+    const sql =
       'INSERT INTO issue (title, author_no, milestone_no) VALUES (?, ?, ?);';
-    return pool.execute(issueSql, [title, authorNo, milestoneNo]);
+    return pool.execute(sql, [title, authorNo, milestoneNo]);
+  },
+  getIssuesLableList() {
+    const sql = `SELECT il.issue_no as issueNo, l.no, l.name, l.description, l.color 
+      FROM issue_label il 
+      LEFT JOIN label l ON il.label_no = l.no 
+      ORDER BY il.issue_no, l.no;`;
+    return pool.query(sql);
+  },
+  getIssuesAssigneeList() {
+    const sql = `SELECT a.issue_no as issueNo, a.user_no as userNo, u.nickname as nickname, u.pw, u.image 
+      FROM assignee a 
+      LEFT JOIN user u ON u.no = a.user_no
+      ORDER BY a.issue_no, u.no;`;
+    return pool.query(sql);
   },
   changeIssueTitle({}) {},
   addIssueAssignee({}) {},
--- backend/routes/api/issues/issues.controller.js
+++ backend/routes/api/issues/issues.controller.js
@@ -1,13 +1,53 @@
+import { issueModel } from '@models';
 /**
  * GET /api/issues
  */
-export const getIssues = (req, res, next) => {
+export const getIssues = async (req, res, next) => {
   const { open, author, assignee, milestone, comment, label } = req.query;
+
+  const [issues] = await issueModel.getIssueList();
+  const [labelList] = await issueModel.getIssuesLableList();
+  const [assigneeList] = await issueModel.getIssuesAssigneeList();
+
+  console.log('label', labelList);
+
+  let labelIdx = 0;
+  let assigneeIdx = 0;
+
+  // TODO : 이 부분 서비스 코드로 분리하기
+  const issuesWithLabelsAndMileStones = issues.map(issue => {
+    const { no: issueNo } = issue;
+    const labels = [];
+    const assignees = [];
+    while (true) {
+      if (labelIdx >= labelList.length) break;
+      const label = labelList[labelIdx];
+      const { no, name, description, color } = label;
+
+      if (+label.issueNo !== +issueNo) break;
+      labels.push({ no, name, description, color });
+      labelIdx += 1;
+    }
+
+    while (true) {
+      if (assigneeIdx >= assigneeList.length) break;
+      const assignee = assigneeList[assigneeIdx];
+      const { userNo: no, nickname, image } = assignee;
+
+      if (+assignee.issueNo !== +issueNo) break;
+      assignees.push({ no, nickname, image });
+      assigneeIdx += 1;
+    }
+    return { ...issue, labels, assignees };
+  });
+
+  // TODO : 여기에 필터조건 걸기
+  const filterdIssues = issuesWithLabelsAndMileStones.filter(i => i);
 
   // TODO : 해당 조건으로 검색하는 로직 구현
   // 검색 조건 + '검색어'로 검색이 가능해야함!
 
-  res.json({ issues: [] });
+  res.json({ issues: filterdIssues });
 };
 
 /**
