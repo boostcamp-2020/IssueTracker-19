4d71022d1caeeb50242ed47388051bbf1af9618b
Front-line-dev <kimminsung@kakao.com>
2020-10-29 18:06:58 +0900

    [style] 쿼리문 세미콜론, 기본값 변경
    isHead = '0' 을 0으로

--- backend/models/comment.model.js
+++ backend/models/comment.model.js
@@ -10,20 +10,20 @@ export const commentModel = {
     ORDER BY issue_no;`;
     return pool.query(sql);
   },
-  addComment({ issueNo, userNo, content, isHead = '0' }) {
-    const sql = `INSERT INTO comment (issue_no, author_no, content, is_head) VALUES (?, ?, ?, ?)`;
+  addComment({ issueNo, userNo, content, isHead = 0 }) {
+    const sql = `INSERT INTO comment (issue_no, author_no, content, is_head) VALUES (?, ?, ?, ?);`;
     return pool.execute(sql, [issueNo, userNo, content, isHead]);
   },
   changeComment({ no, content }) {
-    const sql = `UPDATE comment SET content=? WHERE no=?`;
+    const sql = `UPDATE comment SET content=? WHERE no=?;`;
     return pool.execute(sql, [content, no]);
   },
   deleteComment({ no }) {
-    const sql = `DELETE FROM comment WHERE no=?`;
+    const sql = `DELETE FROM comment WHERE no=?;`;
     return pool.execute(sql, [no]);
   },
   getCommentAuthor({ no }) {
-    const sql = `SELECT author_no as authorNo FROM comment WHERE no=?`;
+    const sql = `SELECT author_no as authorNo FROM comment WHERE no=?;`;
     return pool.execute(sql, [no]);
   },
 };
