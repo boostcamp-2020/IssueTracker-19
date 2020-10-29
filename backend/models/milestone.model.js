1bad93e8d7ca5030a2d122bc0539ead851d266ec
yellow2041 <yellow2041@naver.com>
2020-10-29 13:17:00 +0900

    [refactor] 마일스톤 select 쿼리에 title 추가

--- backend/models/milestone.model.js
+++ backend/models/milestone.model.js
@@ -2,8 +2,7 @@ import pool from '@lib/db';
 
 export const milestoneModel = {
   getMilestoneList() {
-    const sql =
-      `SELECT M.no, M.is_closed AS isClosed, M.is_deleted AS isDeleted, M.due_date AS dueDate, M.description,
+    const sql = `SELECT M.no, M.title, M.is_closed AS isClosed, M.is_deleted AS isDeleted, M.due_date AS dueDate, M.description,
       (SELECT COUNT(*) FROM issue WHERE is_opened=0 AND milestone_no=M.no) AS closedTasks, 
       (SELECT COUNT(*) FROM issue WHERE milestone_no=M.no) AS totalTasks 
       FROM milestone AS M 
@@ -13,8 +12,7 @@ export const milestoneModel = {
     return pool.query(sql);
   },
   getMilestone({ no }) {
-    const sql =
-      `SELECT M.no, M.is_closed AS isClosed, M.is_deleted AS isDeleted, M.due_date AS dueDate, M.description, 
+    const sql = `SELECT M.no, M.title, M.is_closed AS isClosed, M.is_deleted AS isDeleted, M.due_date AS dueDate, M.description, 
       (SELECT COUNT(*) FROM issue WHERE milestone_no = ? AND is_opened=0 ) AS closedTasks,
       (SELECT COUNT(*) FROM issue WHERE milestone_no = ?) AS totalTasks 
       FROM milestone AS M 
@@ -35,15 +33,15 @@ export const milestoneModel = {
     return pool.execute(sql, [title, description, dueDate, no]);
   },
   deleteMilestone({ no }) {
-    const sql = 'UPDATE milestone SET is_deleted=1  WHERE no=?;';
+    const sql = 'UPDATE milestone SET is_deleted=1 WHERE no=?;';
     return pool.execute(sql, [no]);
   },
   openMilestone({ no }) {
-    const sql = 'UPDATE milestone SET is_closed=0 WHERE no=?;'
+    const sql = 'UPDATE milestone SET is_closed=0 WHERE no=?;';
     return pool.execute(sql, [no]);
   },
   closeMilestone({ no }) {
-    const sql = 'UPDATE milestone SET is_closed=1 WHERE no=?;'
+    const sql = 'UPDATE milestone SET is_closed=1 WHERE no=?;';
     return pool.execute(sql, [no]);
   },
 };
