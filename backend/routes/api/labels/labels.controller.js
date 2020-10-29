fa1df98023930bd59b5a966acbeca678fcc2b736
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-10-30 01:43:57 +0900

    [feat] 레이블 관련 API구현 완료
    레이블 관련 모델과 컨트롤러 구현후 포트맨으로 테스트 완료하였습니다.

--- backend/routes/api/labels/labels.controller.js
+++ backend/routes/api/labels/labels.controller.js
@@ -1,34 +1,52 @@
+import { labelModel } from '@models';
 /**
  * GET /api/labels
  */
-export const getLabels = (req, res, next) => {
-  // TODO : 로직 작성
-  res.json({ lables: [] });
+export const getLabels = async (req, res, next) => {
+  try {
+    const [row] = await labelModel.getLabels();
+    res.json({ lables: row });
+  } catch (err) {
+    next(err);
+  }
 };
 
 /**
  * POST	/api/labels
  */
-export const addLabel = (req, res, next) => {
-  const { name, description, color } = req.body;
-  // TODO : 로직 작성
-  res.status(201).end();
+export const addLabel = async (req, res, next) => {
+  try {
+    const { name, description, color } = req.body;
+    await labelModel.addLabel({ name, description, color });
+    res.status(201).end();
+  } catch (err) {
+    next(err);
+  }
 };
 
 /**
  * PUT /api/labels/:no
  */
-export const changeLabel = (req, res, next) => {
-  const { no } = req.params;
-  // TODO : 로직 작성
-  res.status(200).end();
+export const changeLabel = async (req, res, next) => {
+  try {
+    const { no } = req.params;
+    const { name, description, color } = req.body;
+    await labelModel.changeLabel({ name, description, color, no });
+    res.status(200).end();
+  } catch (err) {
+    next(err);
+  }
 };
 
 /**
  * DELETE /api/labels/:no
  */
-export const deleteLabel = (req, res, next) => {
-  const { no } = req.params;
-  // TODO : 로직 작성
-  res.status(200).end();
+export const deleteLabel = async (req, res, next) => {
+  try {
+    const { no } = req.params;
+    await labelModel.deleteLabel({ no });
+    res.status(200).end();
+  } catch (err) {
+    next(err);
+  }
 };
