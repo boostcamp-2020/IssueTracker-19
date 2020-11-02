b965530c08d0ae4fc29b0f603993b9416003d5ac
rnjshippo <tkstka0023@naver.com>
2020-11-03 01:24:50 +0900

    [fix] verify 함수가 잘못 적용된 코드 롤백
    해당 컨트롤러에서 milestoneNo는 null이어도 되므로 verify 함수를 실행하지 않아야됨.

--- backend/routes/api/issues/[no]/milestone/milestone.controller.js
+++ backend/routes/api/issues/[no]/milestone/milestone.controller.js
@@ -1,6 +1,4 @@
 import { issueModel } from '@models';
-import { verify } from '@lib/utils';
-import { errorMessage } from '@lib/constants';
 
 /**
  * PATCH /api/issues/:no/milestone
@@ -9,12 +7,8 @@ export const changeMilestone = async (req, res, next) => {
   try {
     const { no } = req.params;
     const { milestoneNo } = req.body;
-    if (verify([milestoneNo])) {
-      await issueModel.changeIssueMilestone({ no, milestoneNo });
-      res.status(200).end();
-      return;
-    }
-    res.status(400).json({ message: errorMessage.MISSING_REQUIRED_VALUE });
+    await issueModel.changeIssueMilestone({ no, milestoneNo });
+    res.status(200).end();
   } catch (err) {
     next(err);
   }
