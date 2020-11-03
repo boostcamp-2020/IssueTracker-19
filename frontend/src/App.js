dbe68cd4d9ec129e0e76f5d5849ef35091eb2a1b
rnjshippo <tkstka0023@naver.com>
2020-11-03 20:41:55 +0900

    [feat] 페이지별 라우팅 적용
    프론트 라우팅 적용 -> url에 따라 필요한 페이지 컴포넌트를 렌더링

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
-import { API } from '@api';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { NotFound, Issue, Login } from './pages';
 
 const App = () => {
-  const handleLogout = async e => {
-    e.preventDefault();
-    const { data, status } = await API.post('/api/auth/logout');
-    console.log('logout', data, status);
-  };
-
   return (
-    <div className="container">
-      <Button>
-        <a href="/api/auth/github">github 로그인</a>
-      </Button>
-      <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/issues" exact component={Issue} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 };
-
-const Button = styled.button`
-  padding: 1rem;
-  margin: 1rem;
-  background-color: #29b6af;
-  border: none;
-  color: white;
-  display: block;
-  cursor: pointer;
-`;
-
-const LogoutButton = styled.button`
-  padding: 1rem;
-  margin: 1rem;
-  color: #29b6af;
-  border: 1px solid #29b6af;
-  background-color: white;
-  display: block;
-  cursor: pointer;
-`;
 
 export default App;
