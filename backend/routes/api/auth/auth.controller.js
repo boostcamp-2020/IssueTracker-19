e3dfc78df3b14f4f3e2a929c3a9f83089236dfb4
rnjshippo <tkstka0023@naver.com>
2020-11-03 17:00:57 +0900

    [feat] 회원가입 구현
    1. 회원가입 구현
    2. userModel의 addUser 함수에서 pw의 default를 null로 지정(OAuth의 경우 비번 필요 x)

--- backend/routes/api/auth/auth.controller.js
+++ backend/routes/api/auth/auth.controller.js
@@ -1,4 +1,8 @@
 import passport from 'passport';
+import { verify } from '@lib/utils';
+import { errorMessage } from '@lib/constants';
+import { userModel } from '@models';
+import bcrypt from 'bcrypt';
 
 /* 
 	POST /api/auth/login
@@ -31,4 +35,23 @@ export const logout = async (req, res) => {
   req.session.save(() => {
     res.status(200).end();
   });
+};
+
+/*
+	POST /api/auth/signup
+*/
+export const signup = async (req, res, next) => {
+  try {
+    const { id, nickname, pw, auth } = req.body;
+    if (verify([id, nickname, pw, auth])) {
+      const hashPw = await bcrypt.hash(pw, 10);
+      await userModel.addUser({ id, nickname, pw: hashPw, auth });
+      res.status(200).end();
+      return;
+    }
+    res.status(400).json({ message: errorMessage.MISSING_REQUIRED_VALUE });
+  } catch (err) {
+    next(err);
+  }
+  // TODO signup 구현
 };
