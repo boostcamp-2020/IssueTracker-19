34f3253fdcac64339854fdd513cc9ec6ca6158de
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-11-05 00:49:30 +0900

    [ feat ] 마일스톤 input부분 마크업

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header/Header';
 export { default as LoginBox } from './LoginBox/LoginBox';
 export { default as SignUpBox } from './SignUpBox/SignUpBox';
+export { default as MilestoneEditBox } from './MilestoneEditBox/MilestoneEditBox';
---
+++ frontend/src/pages/MilestoneNew.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header, MilestoneEditBox } from '@components';
+
+export default function MilestoneNew() {
+  return (
+    <>
+      <Header />
+      <MilestoneEditBox />
+    </>
+  );
+}
