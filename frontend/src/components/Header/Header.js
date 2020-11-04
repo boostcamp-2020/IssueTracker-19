a5b53b683e4aa1b9fd321ba209151014d32cf331
Front-line-dev <kimminsung@kakao.com>
2020-11-04 15:46:50 +0900

    [refactor] export 컨벤션 변경

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { flex } from '@styles/utils';
+import { flexCenter } from '@styles/utils';
 
 const HeaderBox = styled.header`
   width: 100%;
   height: 4rem;
   color: white;
   background-color: var(--black3, #333);
-  ${flex()}
+  ${flexCenter}
 `;
 
-const Header = () => {
+export default function Header() {
   return (
     <HeaderBox>
       <Link to="/">Issue Tracker</Link>
     </HeaderBox>
   );
-};
-
-export default Header;
+}
