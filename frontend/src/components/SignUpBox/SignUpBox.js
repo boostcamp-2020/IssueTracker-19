debe0fd7bbc2c346ccedb6b2751676f5622edf59
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-11-04 20:18:27 +0900

    [refactor] signup css변경 및 label태그 추가

--- frontend/src/components/SignUpBox/SignUpBox.js
+++ frontend/src/components/SignUpBox/SignUpBox.js
@@ -14,28 +14,27 @@ const SignUpArea = styled.div`
 const Title = styled.h1`
   color: #2d3436;
   margin-top: 2rem;
-  font-size: 2.5rem;
+  font-size: 1.6rem;
 `;
-const Box = styled.form`
-  width: 35%;
+const Form = styled.form`
+  width: 20rem;
   margin-top: 2rem;
+  padding: 1rem 0;
   ${flexColumn}
   align-items: center;
   background-color: white;
   border-radius: 5px;
   border: 1px #b2bec3 solid;
 `;
-const Input = styled.div`
+const InputBox = styled.div`
   width: 80%;
   padding: 0rem 1.5rem;
+  margin-top: 1rem;
 `;
-const InputText = styled.div`
-  padding: 1.5rem 1rem 0.5rem 0rem;
-`;
-const InputTextBox = styled.input`
+
+const Input = styled.input`
   width: 100%;
   height: 2rem;
-  margin: 0rem 0rem;
   padding-left: 0.5em;
   border-radius: 3px;
   border: 1px #b2bec3 solid;
@@ -43,18 +42,17 @@ const InputTextBox = styled.input`
 const SignUpButton = styled.button`
   width: 80%;
   padding: 0.7rem;
-  margin: 2rem;
+  margin: 1rem;
   background-color: #27ae60;
   border: none;
   border-radius: 3px;
   color: white;
-  font-size: 1.3rem;
+  font-size: 1rem;
   cursor: pointer;
 `;
 
 export default function SignUpBox() {
   const [form, setForm] = useState({ id: '', nickname: '', pw: '' });
-
   const { id, nickname, pw } = form;
 
   const history = useHistory();
@@ -63,42 +61,48 @@ export default function SignUpBox() {
     const { name, value } = target;
     setForm({ ...form, [name]: value });
   };
-  const submitData = async e => {
-    e.preventDefault();
 
-    const data = {
-      id,
-      nickname,
-      pw,
-      auth: AUTH.DEFAULT,
-    };
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const data = { id, nickname, pw, auth: AUTH.DEFAULT };
     try {
       const { status } = await API.post('/api/auth/signup', data);
       if (status === 200) {
         history.push('/login');
       }
+      // TODO 200 이외의 코드 에러 처리
     } catch (err) {
       console.error(err);
     }
   };
+
   return (
     <SignUpArea>
       <Title>회원가입</Title>
-      <Box>
-        <Input>
-          <InputText>아이디</InputText>
-          <InputTextBox type="text" name="id" onChange={handleChange} value={id} />
-        </Input>
-        <Input>
-          <InputText>닉네임</InputText>
-          <InputTextBox type="text" name="nickname" onChange={handleChange} value={nickname} />
-        </Input>
-        <Input>
-          <InputText>비밀번호</InputText>
-          <InputTextBox type="password" name="pw" onChange={handleChange} value={pw} />
-        </Input>
-        <SignUpButton onClick={submitData}>회원가입</SignUpButton>
-      </Box>
+      <Form onSubmit={handleSubmit}>
+        <InputBox>
+          <label>
+            아이디
+            <Input type="text" name="id" onChange={handleChange} value={id} required />
+          </label>
+        </InputBox>
+        <InputBox>
+          <label>닉네임</label>
+          <Input type="text" name="nickname" onChange={handleChange} value={nickname} required />
+        </InputBox>
+        <InputBox>
+          <label>비밀번호</label>
+          <Input
+            type="password"
+            name="pw"
+            onChange={handleChange}
+            value={pw}
+            autoComplete="true"
+            required
+          />
+        </InputBox>
+        <SignUpButton>회원가입</SignUpButton>
+      </Form>
     </SignUpArea>
   );
 }
