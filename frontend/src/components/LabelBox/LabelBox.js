e081fb1accce206ed574fdfa53cb72270b50a0ae
Front-line-dev <kimminsung@kakao.com>
2020-11-05 13:03:56 +0900

    [fix] LabelMilestoneTab 삭제
    공통 컴포넌트를 사용해서 새로 만들어야 함

--- frontend/src/components/LabelBox/LabelBox.js
+++ frontend/src/components/LabelBox/LabelBox.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { LabelMilestoneTab } from '@components/Common';
 import LabelEditBox from './LabelEditBox/LabelEditBox';
 
 export default function LabelBox() {
   return (
     <>
-      <LabelMilestoneTab />
       <LabelEditBox />
     </>
   );
