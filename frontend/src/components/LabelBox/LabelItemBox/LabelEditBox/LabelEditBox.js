1c4e6c8746cf67aafc6295c67d2c3c068e7dab41
Front-line-dev <kimminsung@kakao.com>
2020-11-05 17:55:52 +0900

    [feat] Label Edit Box 기능 마크업

--- frontend/src/components/LabelBox/LabelItemBox/LabelEditBox/LabelEditBox.js
+++ frontend/src/components/LabelBox/LabelItemBox/LabelEditBox/LabelEditBox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { flex, flexColumn } from '@styles/utils';
+import { flex, flexColumn, calFontColor } from '@styles/utils';
 import { colors } from '@styles/variables';
 
 const Box = styled.div`
@@ -16,11 +16,23 @@ const EditBody = styled.div`
   ${flex('space-between')}
 `;
 
-export default function LabelEditBox() {
+const ItemBox = styled.div`
+  ${flexColumn}
+`;
+
+export default function LabelEditBox({ no, name, description, color }) {
   return (
     <Box>
-      <EditHeader />
-      <EditBody />
+      <EditHeader>
+        <ItemBox>{/* 레이블 미리보기 */}</ItemBox>
+        <ItemBox>{/* 지우기 버튼 */}</ItemBox>
+      </EditHeader>
+      <EditBody>
+        <ItemBox>{/* 이름 */}</ItemBox>
+        <ItemBox>{/* 설명 */}</ItemBox>
+        <ItemBox>{/* 색 */}</ItemBox>
+        <ItemBox>{/* 버튼들 */}</ItemBox>
+      </EditBody>
     </Box>
   );
 }
