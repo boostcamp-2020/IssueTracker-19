1fcf35ec9bbe539b8f40271926bdad9ad3bdd25d
Front-line-dev <kimminsung@kakao.com>
2020-11-05 17:37:38 +0900

    [fix] 무한 API fetch 버그 픽스
    컴포넌트가 다시 렌더링 안 시켜도 무한으로 즐기는지 몰랐습니다 ㅠㅠ

--- frontend/src/components/LabelBox/LabelItemBox/LabelItemBox.js
+++ frontend/src/components/LabelBox/LabelItemBox/LabelItemBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { API } from '@api';
 import styled from 'styled-components';
 import { colors } from '@styles/variables';
@@ -21,24 +21,31 @@ const LabelHeader = styled.div`
   }
 `;
 
-const getLabels = async () => {
-  try {
-    const { data, status } = await API.get('/api/labels');
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 const tempLabels = JSON.parse(
   '{"labels":[{"no":1,"name":"backend","description":"backend 작업","color":"#FFFFFF"},{"no":2,"name":"frontend","description":"frontend 작업","color":"#333333"}]}',
 );
 
 export default function LabelItemBox() {
+  const [fetched, setFetched] = useState(false);
+  const [labels, setLabels] = useState([]);
+
+  const getLabels = async () => {
+    try {
+      const { data, status } = await API.get('/api/labels');
+      setFetched(true);
+      setLabels(data.labels);
+    } catch (err) {
+      console.log(err);
+      // TODO : if error 401, redirect to login page
+    }
+  };
+
+  if (!fetched) getLabels();
+
   return (
     <Box>
       <LabelHeader>8 Labels</LabelHeader>
-      {tempLabels.labels.map(label => (
+      {labels.map(label => (
         <LabelItem key={label.no} {...label} />
       ))}
     </Box>
