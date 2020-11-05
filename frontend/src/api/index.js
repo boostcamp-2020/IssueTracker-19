68c922a4234d8562576266d7fdb99383d8c05c4c
rnjshippo <tkstka0023@naver.com>
2020-11-05 18:25:18 +0900

    [fix] axios baseUrl 삭제

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const jsonHeader = { 'Content-Type': 'application/json;charset=utf-8' };
-const instance = axios.create({ baseURL: 'http://localhost:3000' });
+const instance = axios.create();
 
 instance.interceptors.request.use(
   config => {
