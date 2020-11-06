a77bdc09d0b80012e689f3f8f56b6f01540aef1f
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-11-07 01:00:16 +0900

    [feat] 마일스톤 리스트 페이지 생성
    아직 마크업은 안함

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,5 +3,6 @@ export { default as Issue } from './Issue';
 export { default as Login } from './Login';
 export { default as SignUp } from './SignUp';
 export { default as Label } from './Label';
+export { default as Milestone } from './Milestone';
 export { default as MilestoneNew } from './MilestoneNew';
 export { default as MilestoneEdit } from './MilestoneEdit';
