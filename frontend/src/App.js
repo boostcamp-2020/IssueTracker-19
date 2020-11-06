88eb98922c6e796c0b60c2f9e1533efff182e15b
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-11-06 23:45:30 +0900

    [temp] rebase를 위한 임시커밋

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { NotFound, Issue, Login, SignUp, Label, MilestoneNew, MilestoneEdit } from './pages';
+import {
+  NotFound,
+  Issue,
+  Login,
+  SignUp,
+  Label,
+  Milestone,
+  MilestoneNew,
+  MilestoneEdit,
+} from './pages';
 
 const App = () => {
   return (
@@ -10,6 +19,7 @@ const App = () => {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
         <Route path="/labels" component={Label} />
+        <Route path="/milestones" exact componet={Milestone} />
         <Route path="/milestones/new" component={MilestoneNew} />
         <Route path="/milestones/:no" component={MilestoneEdit} />
         <Route component={NotFound} />
