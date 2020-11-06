f9caf801d8bb21e8d99e9c797bb670d54004bf6c
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-11-07 00:59:34 +0900

    [refactor] 마일스톤 관련 페이지 margin 수정 및 Tab영역 추가

--- frontend/src/pages/Milestone.js
+++ frontend/src/pages/Milestone.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Header } from '@components';
+import styled from 'styled-components';
+import { Header, LabelMilestoneTab } from '@components';
+import { numerics } from '@styles/variables';
+
+const Container = styled.div`
+  margin: 32px ${numerics.marginHorizontal};
+`;
 
 export default function Milestone() {
   return (
     <>
       <Header />
+      <Container>
+        <LabelMilestoneTab Submit={true} ButtonName={'New milestone'} />
+      </Container>
     </>
   );
 }
