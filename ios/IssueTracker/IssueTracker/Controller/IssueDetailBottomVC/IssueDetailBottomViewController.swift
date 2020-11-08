dfd67a8b9efd2f4b5418ec7167c863d2a491a066
gihyun cho <whrlgus33@gmail.com>
2020-11-08 22:46:38 +0900

    temporary commit for add view for user interaction

--- ios/IssueTracker/IssueTracker/Controller/IssueDetailBottomVC/IssueDetailBottomViewController.swift
+++ ios/IssueTracker/IssueTracker/Controller/IssueDetailBottomVC/IssueDetailBottomViewController.swift
@@ -29,7 +29,6 @@ class IssueDetailBottomViewController: UIViewController {
 	@IBOutlet weak var collectionView: UICollectionView!
 	var dataSource: DataSource!
 	
-	
 	// MARK: Variables for determining view frame
 	var statusBarHeight: CGFloat {
 		let height: CGFloat
@@ -48,12 +47,10 @@ class IssueDetailBottomViewController: UIViewController {
 	var heightConstraint: NSLayoutConstraint?
 	var isBelow = true
 	
-	
 	// MARK: Collection view's section items
 	var assignees = [User]()
 	var labels = [Label]()
 	var milestones = [Milestone]()
-	
 	
 	// MARK: - life cycle
 	override func viewDidLoad() {
@@ -66,6 +63,7 @@ class IssueDetailBottomViewController: UIViewController {
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
+		
 		showViewWithAnimation()
 	}
 	
@@ -112,8 +110,11 @@ class IssueDetailBottomViewController: UIViewController {
 		guard let editSectionItemVC = storyboard?.instantiateViewController(identifier: "editSectionItemVC") as? SectionItemEditViewController else { return }
 		
 		addChild(editSectionItemVC)
+		editSectionItemVC.view.frame = view.bounds
 		view.addSubview(editSectionItemVC.view)
 		editSectionItemVC.didMove(toParent: self)
+		
+		view.isUserInteractionEnabled = false
 	}
 	
 	func closeButtonAction() {
@@ -141,9 +142,9 @@ extension IssueDetailBottomViewController {
 	}
 	
 	func createLayout() -> UICollectionViewLayout {
-		return UICollectionViewCompositionalLayout { (sectionIdx, layoutEnv) -> NSCollectionLayoutSection? in
+		return UICollectionViewCompositionalLayout { [weak self] sectionIdx, _ in
 			guard let type = SectionType(rawValue: sectionIdx) else { return nil }
-			return self.createLayoutSection(type: type)
+			return self?.createLayoutSection(type: type)
 		}
 	}
 	
@@ -181,8 +182,8 @@ extension IssueDetailBottomViewController {
 		return section
 	}
 	
-	func configureDataSource() {
-		dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, item in
+	func createDataSource() -> DataSource {
+		DataSource(collectionView: collectionView) { collectionView, indexPath, item in
 			var identifier = ""
 			switch SectionType(rawValue: indexPath.section) {
 			case .assignee:
@@ -202,8 +203,12 @@ extension IssueDetailBottomViewController {
 			cell.item = item
 			return cell
 		}
+	}
+	
+	func configureDataSource() {
+		dataSource = createDataSource()
 		
-		dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+		dataSource.supplementaryViewProvider = { [weak self] collectionView, kind, indexPath in
 			if kind == UICollectionView.elementKindSectionHeader {
 				guard let headerView = collectionView.dequeueReusableSupplementaryView(
 					ofKind: kind,
@@ -211,7 +216,7 @@ extension IssueDetailBottomViewController {
 					for: indexPath) as? BottomViewHeaderView
 				else { return nil }
 				headerView.sectionIdx = indexPath.section
-				headerView.closure = self.sectionHeaderEditButtonAction
+				headerView.closure = self?.sectionHeaderEditButtonAction
 				headerView.titleLabel.text = SectionType(rawValue: indexPath.section)?.text
 				return headerView
 			} else {
@@ -220,11 +225,15 @@ extension IssueDetailBottomViewController {
 					withReuseIdentifier: BottomViewFooterView.identifier,
 					for: indexPath) as? BottomViewFooterView
 				else { return nil }
-				footerView.closure = self.closeButtonAction
+				footerView.closure = self?.closeButtonAction
 				return footerView
 			}
 		}
 		
+		applySnapshot()
+	}
+	
+	func applySnapshot() {
 		var snapshot = NSDiffableDataSourceSnapshot<SectionType, HashableObject>()
 		snapshot.appendSections([.assignee])
 		snapshot.appendItems(assignees)
@@ -242,6 +251,7 @@ extension IssueDetailBottomViewController {
 	func configureScrollAction() {
 		let gesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture))
 		view.addGestureRecognizer(gesture)
+		
 		NotificationCenter.default.addObserver(self,
 											   selector: #selector(rotated),
 											   name: UIDevice.orientationDidChangeNotification,
@@ -252,6 +262,7 @@ extension IssueDetailBottomViewController {
 		let translation = recognizer.translation(in: view)
 		let posY = view.frame.minY
 		let nPosY = posY + translation.y
+		print(posY)
 		if nPosY < maxTop || minTop < nPosY { return }
 		
 		self.view.frame.origin.y = nPosY
--- ios/IssueTracker/IssueTracker/Controller/IssueDetailBottomVC/SectionItemEditViewController.swift
+++ ios/IssueTracker/IssueTracker/Controller/IssueDetailBottomVC/SectionItemEditViewController.swift
@@ -9,22 +9,75 @@
 import UIKit
 
 class SectionItemEditViewController: UIViewController {
+	
+	let topSpace: CGFloat = 10
+	var height: CGFloat { view.frame.height }
+	
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		configureScrollAction()
     }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		view.frame.size.height = height + 10
+		view.frame.origin.y = height
+		UIView.animate(withDuration: 0.4) { [weak self] in
+			guard let controller = self else { return }
+			controller.view.frame.origin.y = controller.topSpace
+		}
+	}
     
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		view.isUserInteractionEnabled = true
+	}
+	
+	func remove() {
+		willMove(toParent: nil)
+		view.removeFromSuperview()
+		removeFromParent()
+	}
 
-    /*
-    // MARK: - Navigation
+	func configureScrollAction() {
+		let gesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture))
+		view.addGestureRecognizer(gesture)
+	}
+	
+	@objc func panGesture(_ recognizer: UIPanGestureRecognizer) {
+		let translation = recognizer.translation(in: view)
+		let posY = view.frame.minY
+		var nPosY = posY + translation.y
+		if nPosY < topSpace && translation.y < 0 {
+			nPosY -= (translation.y + 2)
+		}
+		if nPosY < -topSpace {
+//			recognizer.state = .ended
+			nPosY = -topSpace
+		}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+		self.view.frame.origin.y = nPosY
+		recognizer.setTranslation(.zero, in: view)
+		
+
+		let velocity = recognizer.velocity(in: view)
+		if recognizer.state == .ended {
+			UIView.animate(withDuration: 0.4) { [weak self] in
+				guard let controller = self else { return }
+				if velocity.y > 400 {
+					controller.view.frame.origin.y = controller.height
+				} else {
+					controller.view.frame.origin.y = nPosY < controller.height / 2 ?
+						controller.topSpace : controller.height
+				}
+			} completion: { [weak self] _ in
+				guard let controller = self else { return }
+				if controller.view.frame.origin.y == controller.height {
+					controller.remove()
+				}
+			}
+		}
+	}
 
 }
