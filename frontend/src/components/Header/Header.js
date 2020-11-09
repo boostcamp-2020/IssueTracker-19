75174bece2c0079999fce8468cde1ccfebbfee98
rnjshippo <tkstka0023@naver.com>
2020-11-09 16:56:40 +0900

    [feat] Header flex-shrink 수정

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { flexCenter } from '@styles/utils';
+import { colors } from '@styles/variables';
 
 const HeaderBox = styled.header`
   width: 100%;
   height: 4rem;
   color: white;
-  background-color: var(--black3, #333);
+  background-color: ${colors.black3};
   ${flexCenter}
+  flex-shrink:0;
 `;
 
 export default function Header() {
