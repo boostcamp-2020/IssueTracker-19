55f42154c5fff83c73c4a05a20b53384ada6be67
rnjshippo <tkstka0023@naver.com>
2020-11-11 01:56:32 +0900

    [feat] withAuth hoc에서 user정보 주입

--- frontend/src/hoc/withAuth.js
+++ frontend/src/hoc/withAuth.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { userService } from '@services';
 import { useHistory } from 'react-router-dom';
 
 export default function withAuth(InnerComponent) {
   return function WrapperComponent() {
     const history = useHistory();
+    const [user, setUser] = useState(null);
 
     const checkLogin = async () => {
       try {
         const { data, status } = await userService.checkLogin();
+        if (status === 200) {
+          setUser(data.user);
+        }
       } catch (err) {
         history.push('/login');
       }
@@ -18,6 +22,6 @@ export default function withAuth(InnerComponent) {
       checkLogin();
     }, []);
 
-    return <InnerComponent />;
+    return <InnerComponent user={user} />;
   };
 }
