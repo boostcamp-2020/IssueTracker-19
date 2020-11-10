ded885bb930e52331606b246e2334d75704eb739
Front-line-dev <kimminsung@kakao.com>
2020-11-10 13:07:26 +0900

    [feat] 컬러 체인지 버튼 svg fill 색상 적용
    calcFontColor 함수를 통해 자동으로 적절한 색상을 선택하도록 함

--- frontend/src/components/LabelBox/LabelItemBox/LabelEditBox/LabelEditBox.js
+++ frontend/src/components/LabelBox/LabelItemBox/LabelEditBox/LabelEditBox.js
@@ -4,6 +4,7 @@ import { flex, flexColumn, calFontColor } from '@styles/utils';
 import { colors } from '@styles/variables';
 import { LabelTag } from '@components';
 import { SubmitButton, CancelButton } from '@shared';
+import { calcFontColor } from '@styles/utils';
 
 const Box = styled.div`
   ${flexColumn};
@@ -84,7 +85,7 @@ const svgConfig = styled.svg.attrs({
 
 // TODO : fill color adjust
 const ColorIcon = styled(svgConfig)`
-  fill: black;
+  fill: ${props => calcFontColor(props.backgroundColor)};
   width: 16px;
   height: 16px;
 `;
@@ -116,7 +117,7 @@ export default function LabelEditBox({
           <InputLabel>Color</InputLabel>
           <ItemBoxRow>
             <ColorButton backgroundColor={color}>
-              <ColorIcon fill={color}>
+              <ColorIcon backgroundColor={color}>
                 <path
                   fillRule="evenodd"
                   d="M8 2.5a5.487 5.487 0 00-4.131 1.869l1.204 1.204A.25.25 0 014.896 6H1.25A.25.25 0 011 5.75V2.104a.25.25 0 01.427-.177l1.38 1.38A7.001 7.001 0 0114.95 7.16a.75.75 0 11-1.49.178A5.501 5.501 0 008 2.5zM1.705 8.005a.75.75 0 01.834.656 5.501 5.501 0 009.592 2.97l-1.204-1.204a.25.25 0 01.177-.427h3.646a.25.25 0 01.25.25v3.646a.25.25 0 01-.427.177l-1.38-1.38A7.001 7.001 0 011.05 8.84a.75.75 0 01.656-.834z"
