83530738baab9032a3f56c187e84524ea224b499
Front-line-dev <kimminsung@kakao.com>
2020-11-10 13:06:33 +0900

    [refactor] LabelTag 리팩토링
    인라인 스타일 방식에서 스타일드 컴포넌트로

--- frontend/src/components/LabelTag/LabelTag.js
+++ frontend/src/components/LabelTag/LabelTag.js
@@ -5,11 +5,15 @@ import { calcFontColor } from '@styles/utils';
 const Tag = styled.span`
   padding: 0.25em 0.7em;
   border-radius: 999em;
+
+  background-color: ${props => props.color};
+  color: ${props => calcFontColor(props.color)};
+  font-size: ${props => props.size};
 `;
 
 export default function LabelTag({ name, color, size = '1rem' }) {
   return (
-    <Tag style={{ backgroundColor: color, color: calcFontColor(color), fontSize: size }}>
+    <Tag color={color} size={size}>
       {name}
     </Tag>
   );
