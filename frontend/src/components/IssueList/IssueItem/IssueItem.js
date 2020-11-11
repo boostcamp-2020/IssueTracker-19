943d456863372beef0616364bbbd596c6799b8fe
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-11-11 20:27:59 +0900

    [tmp] 임시커밋

--- frontend/src/components/IssueList/IssueItem/IssueItem.js
+++ frontend/src/components/IssueList/IssueItem/IssueItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { colors } from '@styles/variables';
 import { flex } from '@styles/utils';
 import { LabelTag } from '@components';
@@ -7,6 +7,11 @@ import milestoneIcon from '@imgs/milestone.svg';
 
 const Container = styled.div`
   border: 1px solid ${colors.lighterGray};
+  ${flex('space-between', 'center')}
+`;
+
+const IssueDiv = styled.div`
+  width: 65%;
   ${flex('flex-start', 'center')}
 `;
 
@@ -68,6 +73,27 @@ const MilestoneImg = styled.img`
   margin-right: 0.2rem;
 `;
 
+const AssigneeBox = styled.div`
+  ${flex('space-around', 'center')}
+  margin-right: 5rem;
+`;
+
+const AssigneeIcon = styled.div`
+  width: 1.25rem;
+  height: 1.25rem;
+  margin-right: -0.5rem;
+  background-color: #123;
+  border-radius: 50%;
+  &:hover {
+    transition: margin-right ease 0.5s 0s;
+    ${props =>
+      props.move &&
+      css`
+        margin-right: ${props.move};
+      `}
+  }
+`;
+
 export default function IssueItem(props) {
   const {
     isOpened,
@@ -80,8 +106,8 @@ export default function IssueItem(props) {
     no,
     milestoneNo,
     milestoneTitle,
+    assignees,
   } = props;
-
   const handleCheck = ({ target }) => {
     setIssues(
       issues.map(issue => (issue.title === title ? { ...issue, checked: target.checked } : issue)),
@@ -90,47 +116,57 @@ export default function IssueItem(props) {
 
   return (
     <Container>
-      <CheckBox type="checkbox" checked={checked} onChange={handleCheck} />
-      {isOpened ? (
-        <OpenIcon viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-          <path
-            fillRule="evenodd"
-            d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"
-          ></path>
-        </OpenIcon>
-      ) : (
-        <ClosedIcon viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-          <path
-            fillRule="evenodd"
-            d="M1.5 8a6.5 6.5 0 0110.65-5.003.75.75 0 00.959-1.153 8 8 0 102.592 8.33.75.75 0 10-1.444-.407A6.5 6.5 0 011.5 8zM8 12a1 1 0 100-2 1 1 0 000 2zm0-8a.75.75 0 01.75.75v3.5a.75.75 0 11-1.5 0v-3.5A.75.75 0 018 4zm4.78 4.28l3-3a.75.75 0 00-1.06-1.06l-2.47 2.47-.97-.97a.749.749 0 10-1.06 1.06l1.5 1.5a.75.75 0 001.06 0z"
-          ></path>
-        </ClosedIcon>
-      )}
-      <TitleContainer>
-        <TopBox>
-          <Title>{title}</Title>
-          <TagBox>
-            {labels.map(({ no, name, color, description }) => {
-              return (
-                <Label key={no}>
-                  <LabelTag name={name} color={color} size={'0.7rem'}></LabelTag>
-                </Label>
-              );
-            })}
-          </TagBox>
-        </TopBox>
-        <BottomBox>
-          <AuthorBox>
-            #{no} opened by {author}
-          </AuthorBox>
-          {milestoneNo ? (
-            <MilestoneBox>
-              <MilestoneImg src={milestoneIcon} />
-              {milestoneTitle}
-            </MilestoneBox>
-          ) : null}
-        </BottomBox>
-      </TitleContainer>
+      <IssueDiv>
+        <CheckBox type="checkbox" checked={checked} onChange={handleCheck} />
+        {isOpened ? (
+          <OpenIcon viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+            <path
+              fillRule="evenodd"
+              d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"
+            ></path>
+          </OpenIcon>
+        ) : (
+          <ClosedIcon viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+            <path
+              fillRule="evenodd"
+              d="M1.5 8a6.5 6.5 0 0110.65-5.003.75.75 0 00.959-1.153 8 8 0 102.592 8.33.75.75 0 10-1.444-.407A6.5 6.5 0 011.5 8zM8 12a1 1 0 100-2 1 1 0 000 2zm0-8a.75.75 0 01.75.75v3.5a.75.75 0 11-1.5 0v-3.5A.75.75 0 018 4zm4.78 4.28l3-3a.75.75 0 00-1.06-1.06l-2.47 2.47-.97-.97a.749.749 0 10-1.06 1.06l1.5 1.5a.75.75 0 001.06 0z"
+            ></path>
+          </ClosedIcon>
+        )}
+        <TitleContainer>
+          <TopBox>
+            <Title>{title}</Title>
+            <TagBox>
+              {labels.map(({ no, name, color, description }) => {
+                return (
+                  <Label key={no}>
+                    <LabelTag name={name} color={color} size={'0.7rem'}></LabelTag>
+                  </Label>
+                );
+              })}
+            </TagBox>
+          </TopBox>
+          <BottomBox>
+            <AuthorBox>
+              #{no} opened by {author}
+            </AuthorBox>
+            {milestoneNo ? (
+              <MilestoneBox>
+                <MilestoneImg src={milestoneIcon} />
+                {milestoneTitle}
+              </MilestoneBox>
+            ) : null}
+          </BottomBox>
+        </TitleContainer>
+      </IssueDiv>
+      <AssigneeBox>
+        {assignees.map((assignee, idx) => (
+          <AssigneeIcon
+            key={assignee.no}
+            move={'' + 1 * (assignees.length - idx - 1) + 'px'}
+          ></AssigneeIcon>
+        ))}
+      </AssigneeBox>
     </Container>
   );
 }
