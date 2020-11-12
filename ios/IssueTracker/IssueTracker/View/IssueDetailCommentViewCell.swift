c2f3633ed029224c93000e59d02b22ae90e58787
gihyun cho <whrlgus33@gmail.com>
2020-11-13 08:33:50 +0900

    [feat] markdown rendering on issue detail

--- ios/IssueTracker/IssueTracker/View/IssueDetailCommentViewCell.swift
+++ ios/IssueTracker/IssueTracker/View/IssueDetailCommentViewCell.swift
@@ -7,23 +7,26 @@
 //
 
 import UIKit
+import MarkdownView
 
 class IssueDetailCommentViewCell: UICollectionViewCell {
 	static var identifier: String {
 		Self.self.description()
 	}
 
+	@IBOutlet weak var markdownHeight: NSLayoutConstraint!
 	@IBOutlet weak var imageView: UIImageView!
 	@IBOutlet weak var authorLabel: UILabel!
 	@IBOutlet weak var createdAtLabel: UILabel!
-	@IBOutlet weak var contentLabel: UILabel!
+	
+	@IBOutlet weak var mdContentView: UIView!
 	
 	var issueComment: Comment? {
 		didSet {
 			guard let comment = issueComment else { return }
 			authorLabel.text = comment.author
 			createdAtLabel.text = comment.updatedAt
-			contentLabel.text = comment.content
+			
 			HTTPAgent.shared.loadImage(urlString: comment.image ?? "") { [weak self] (result) in
 				switch result {
 				case .success(let path):
@@ -35,6 +38,26 @@ class IssueDetailCommentViewCell: UICollectionViewCell {
 				}
 			}
 			
+		}
+	}
+	
+	weak var wrapper: MarkdownWrapper? {
+		didSet {
+			mdContentView.subviews.forEach{ $0.removeFromSuperview() }
+			markdownHeight.constant = 0
+			guard let wrapper = wrapper, let height = wrapper.height else {
+				return
+			}
+			wrapper.markdown.removeFromSuperview()
+			markdownHeight.constant = height
+			mdContentView.addSubview(wrapper.markdown)
+			wrapper.markdown.translatesAutoresizingMaskIntoConstraints = false
+			NSLayoutConstraint.activate([
+				wrapper.markdown.leadingAnchor.constraint(equalTo: mdContentView.leadingAnchor),
+				wrapper.markdown.topAnchor.constraint(equalTo: mdContentView.topAnchor),
+				wrapper.markdown.bottomAnchor.constraint(equalTo: mdContentView.bottomAnchor),
+				wrapper.markdown.trailingAnchor.constraint(equalTo: mdContentView.trailingAnchor)
+			])
 		}
 	}
 	
