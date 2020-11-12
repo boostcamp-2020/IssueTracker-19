e5694a43d72c423a74460cca12b346dc3c69ee9e
rnjshippo <rnjshippo@github.com>
2020-11-13 01:23:05 +0900

    [fix] deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-git checkout web
+git checkout master
 git fetch
 
 head=`git rev-parse HEAD`
-origin=`git rev-parse origin/web`
+origin=`git rev-parse origin/master`
 
 if [ $head != $origin ]; then
         git pull
-        pm2 reload app
+        cd /root/IssueTracker-19/backend
+        npm i
         cd /root/IssueTracker-19/frontend
+        npm i
         npm run build
+        pm2 reload app
 fi
 echo 'finish!'
