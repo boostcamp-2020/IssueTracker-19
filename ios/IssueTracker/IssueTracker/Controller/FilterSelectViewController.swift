6b03f4247c2f3c1488201908979bc1f3d7584633
NamKiBeom <nkb5813@naver.com>
2020-11-12 21:04:56 +0900

    [feat] issue filter 기능 구현

---
+++ ios/IssueTracker/IssueTracker/Controller/FilterSelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FilterSelectViewController.swift
+//  IssueTracker
+//
+//  Created by 남기범 on 2020/11/12.
+//  Copyright © 2020 남기범. All rights reserved.
+//
+
+import UIKit
+
+class FilterSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
